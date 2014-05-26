628017a0ac471553672b2ad29e195ac1e259f8b9
TZUWEI LO <jeffery.taiwan@gmail.com>
2014-05-27 02:03:21 +0800

    Initial Commit

---
+++ fakenews/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  fakenews
+//
+//  Created by Golo on 14/5/27.
+//  Copyright (c) 2014年 moskastudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ fakenews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fakenews
+//
+//  Created by Golo on 14/5/27.
+//  Copyright (c) 2014年 moskastudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
